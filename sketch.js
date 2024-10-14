f0679d366c680ef9fad522af747232bcc285843f
Christian Alvarez <christian.alvarezr@ucuenca.edu.ec>
2024-10-14 15:20:08 -0500

    100 pelotasas

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+/*
 let posX, posY;
 let velX, velY;
 let diam;
@@ -43,4 +44,58 @@ function draw() {
 
 function changeBackgroundColor() {
     bgColor = color(random(255), random(100), random(255)); // Cambiar a un color aleatorio
+}
+
+*/
+
+let bgColor;
+let pelotas = [];
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  bgColor = color(random(255), random(100), random(255));
+  for (let i = 0; i < 100; i++) {
+    let nuevaPelota = new Pelota();
+    pelotas.push(nuevaPelota);
+  }
+}
+
+function draw() {
+  background(bgColor);
+  for (let i = 0; i < 100; i++) {
+    pelotas[i].update();
+    pelotas[i].display();
+  }
+}
+
+class Pelota {
+  //Plano de instrucciones para crear una pelota
+  constructor() {
+    this.rad = ceil(random(5, 30));
+    this.diam = this.rad * 2;
+    this.posX = random(this.rad, width - this.rad);
+    this.posY = random(this.rad, height - this.rad);
+    this.velX = random(-10, 10);
+    this.velY = random(-10, 10);
+    console.log("estoy vivaaa!");
+  }
+  //método update
+  update() {
+    this.posX += this.velX;
+    this.posY += this.velY;
+
+    if (this.posX > width - this.rad || this.posX < this.rad) {
+      this.velX *= -1;
+    }
+
+    if (this.posY > height - this.rad || this.posY < this.rad) {
+      this.velY *= -1;
+    }
+  }
+
+  display() {
+    fill(255);
+    noStroke();
+    circle(this.posX, this.posY, this.diam);
+  }
 }
